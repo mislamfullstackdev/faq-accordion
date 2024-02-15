7d8781119582aa8b70e006dbc2c8a6e96a9d28f9
mislamfullstackdev <mislam.pasona@gmail.com>
2024-02-15 17:46:48 +0900

    React accordion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import './Question';
 import './App.css';
+import Question from './Question';
+
+
+const questions = [
+  {
+      "id": 1,
+      "title": "What is a healthy diet?",
+      "info": "A healthy diet includes a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats. It's important to limit processed foods, added sugars, and unhealthy fats."
+  },
+  {
+      "id": 2,
+      "title": "How much exercise do I need?",
+      "info": "Adults should aim for at least 150 minutes of moderate-intensity aerobic activity or 75 minutes of vigorous-intensity activity each week, along with muscle-strengthening activities on two or more days per week."
+  },
+  {
+      "id": 3,
+      "title": "How can I manage stress?",
+      "info": "Managing stress involves techniques such as deep breathing exercises, meditation, yoga, spending time outdoors, engaging in hobbies, and seeking support from friends, family, or a mental health professional."
+  },
+  {
+      "id": 4,
+      "title": "What are the benefits of getting enough sleep?",
+      "info": "Getting enough sleep is essential for overall health and well-being. It supports cognitive function, mood regulation, immune function, and helps prevent chronic diseases such as heart disease and diabetes."
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Project 2 : FAQ </h1>
+      <div className="container">
+        <h2> Frequently Asked Questions </h2>
+        <div className="questions">
+          {questions.map((question) => (
+            <Question key={question.id} question={question} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Question.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+// export default function(){}
+
+const Question = ({ question }) => {
+    const [isOpen, setOpen] = useState(false);
+
+    return(
+        <section>
+            <div className={isOpen ? "open" : "closed"}>
+                <h4>{question.title}</h4>
+                <button onClick={() => {setOpen(!isOpen)}}>{isOpen ? "-" : "+"}</button>
+            </div>
+            {isOpen && <p>{question.info}</p>}
+        </section>
+    );
+}
+
+export default Question;
